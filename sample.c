2fa24b5bd1a1e6aa537175e09d292297ec3c561d
214g5a0505 <112407428+214g5a0505@users.noreply.github.com>
2022-08-30 11:52:47 +0530

    harikrishna completed sample.c

---
+++ sample.c
@@ -0,0 +1,12 @@
+/*Name of the candidate:hari krishna
+Roll no:214g5a0505
+Name of the experiment: CPU schedulilng algorithm using FCFS(First Come First Serve).
+Date of Creation:29-08-2022
+Date of Execution:29-08-2022
+*/
+//Source Code
+#include<stdio.h>
+int main()
+{
+printf("Hello World\n");
+}
